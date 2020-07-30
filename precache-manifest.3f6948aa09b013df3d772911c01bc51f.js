6bbe4c6eabb5eb0356af975d29ba73ab580cd713
arbdew <67021813+arbdew@users.noreply.github.com>
2020-07-30 10:52:36 +0100

    Updates

---
+++ precache-manifest.3f6948aa09b013df3d772911c01bc51f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "978fb24976d506306d4b72a537e5f8bf",
+    "url": "/React-Basics/index.html"
+  },
+  {
+    "revision": "1bc463b661d1b38605f0",
+    "url": "/React-Basics/static/css/main.6ae6be22.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/React-Basics/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Basics/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bc463b661d1b38605f0",
+    "url": "/React-Basics/static/js/main.f844b6cf.chunk.js"
+  },
+  {
+    "revision": "43a7b180fb26eeacc7d3",
+    "url": "/React-Basics/static/js/runtime-main.2680dd19.js"
+  }
+]);
